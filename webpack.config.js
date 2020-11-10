6baa7845ea32e0591b0c6dddafc604f7e4e8891a
shwave <50447018+shwave@users.noreply.github.com>
2020-11-10 07:17:29 +0300

    Finished

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,13 @@
 let path = require('path');
 
 module.exports = {
-    mode: 'development', // development production 
+    mode: 'production', 
     entry: './js/script.js', 
     output: {
         filename: 'bundle.js',
         path: __dirname + '/js/dist'
     },
     watch: true, 
-
-    devtool: 'source-map', 
 
     module: { 
         rules: [
